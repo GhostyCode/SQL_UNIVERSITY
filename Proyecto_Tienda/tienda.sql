3cbc543dd991ad48bbf5d2b2ae03548a59fbc24c
Santander Iglesias <santander12342011@hotmail.com>
2023-03-25 17:57:30 -0500

    Procedimiento almacenado de productos

--- Proyecto_Tienda/tienda.sql
+++ Proyecto_Tienda/tienda.sql
@@ -19,7 +19,7 @@ USE `tienda`;
 
 --
 -- Table structure for table `clientes`
--- 
+--
 
 DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -220,9 +220,30 @@ UNLOCK TABLES;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarCliente`(in nom char(60), ape char(60), dire char(60))
+begin
+	INSERT INTO clientes (nombre, apellidos, direccion) 
+VALUES (nom, ape, dire);
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `insertarProductos` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarProductos`(in nom char(60), precioC char(10), precioV char(10), cant int (11))
 begin
-	INSERT INTO clientes (nombre, apellidos, direccion) 
-VALUES (nom, ape, dire);
+	INSERT INTO productos (nombre, apellidos, direccion) 
+VALUES (nom, precioC, precioV, cant);
+
 end ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -239,4 +260,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-22 10:06:19
+-- Dump completed on 2023-03-25 17:55:10
